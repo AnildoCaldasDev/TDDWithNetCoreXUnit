d85176c2f2dbf0b2144ab1c46c8f04ca62ca9ae9
anildk5@hotmail.com <anildk5@hotmail.com>
2020-08-31 14:24:40 -0300

    criado exemplo de calculo de juros compostos com TDD.

---
+++ TDDWithNetCoreXUnit/Src/CursoOnlineTdd.Domain/Models/CalculoFinanceiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoOnlineTdd.Domain.Models
+{
+    public class CalculoFinanceiro
+    {
+
+        public static double CalcularValorComJurosCompostos(double valorEmprestimo, int numeroDeMeses, double percTaxa)
+        {
+
+            //M = C (1+i)t
+
+            var fixedTax = 1 + (percTaxa / 100);
+            var taxRaisedTime = Math.Pow(fixedTax, numeroDeMeses);
+            var mountRounded = Math.Round(valorEmprestimo * taxRaisedTime, 2);
+            return mountRounded;
+
+            //return Math.Round(valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numeroDeMeses), 2);
+        }
+    }
+}
---
+++ TDDWithNetCoreXUnit/Test/CursoOnlineTdd.Domain.Test/ExemploFinancas/CalculoFinanceiroTests.cs
@@ -0,0 +1,51 @@
+﻿using CursoOnlineTdd.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TDDWithNetCoreXUnit.Test.ExemploFinancas
+{
+    public class CalculoFinanceiroTests
+    {
+
+        //[Fact]
+        //public void TesteJurosCompostos_01()
+        //{
+        //    double valor = CalculoFinanceiro.CalcularValorComJurosCompostos(10000, 12, 2);
+        //    Assert.Equal(valor, (double)12682.42);
+        //}
+
+        //[Fact]
+        //public void TesteJurosCompostos_02()
+        //{
+        //    double valor = CalculoFinanceiro.CalcularValorComJurosCompostos(11937.28, 24, 4);
+        //    Assert.Equal(valor, (double)30598.88);
+        //}
+
+        //[Fact]
+        //public void TesteJurosCompostos_03()
+        //{
+        //    double valor = CalculoFinanceiro.CalcularValorComJurosCompostos(15000, 36, 6);
+        //    Assert.Equal(valor, (double)122208.78);
+        //}
+
+
+        //Exemplo implementado mais refatorado com testes voltados a dados em massa.
+        //Crio aqui uma Teoria e nesta teoria serão validados diferente linhas de dados.
+        //Cada Inline Data é um teste diferente.
+        [Theory]
+        [InlineData(10000, 12, 2, 12682.42)]
+        [InlineData(11937.28, 24, 4, 30598.88)]
+        [InlineData(15000, 36, 6, 122208.78)]
+        [InlineData(20000, 36, 6, 162945.04)]
+        [InlineData(25000, 48, 6, 409846.79)]
+        public void TesteJurosCompostos(double valorEmprestimo, int numMeses, double percTaxa, double valorEsperado)
+        {
+            double valor = CalculoFinanceiro.CalcularValorComJurosCompostos(valorEmprestimo, numMeses, percTaxa);
+            Assert.Equal(valor, valorEsperado);
+        }
+
+
+    }
+}
