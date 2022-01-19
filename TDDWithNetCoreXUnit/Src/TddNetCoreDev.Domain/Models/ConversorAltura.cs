9042dc35ea44e436d14b86c81264b3a104961720
anildk5@hotmail.com <anildk5@hotmail.com>
2022-01-19 18:28:10 -0300

    adicionado teste unitário de conversao de altura

---
+++ TDDWithNetCoreXUnit/Src/TddNetCoreDev.Domain/Models/ConversorAltura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TddNetCoreDev.Domain.Models
+{
+
+    public interface IConversorAltura
+    {
+       double PesParaMetros(double pes);
+    }
+
+
+    public class ConversorAltura : IConversorAltura
+    {
+        public double PesParaMetros(double pes)
+        {
+            return Math.Round(pes * 0.3048, 4);
+        }
+    }
+}
---
+++ TDDWithNetCoreXUnit/Test/CursoOnlineTdd.Domain.Test/ConversorAltura/TestesConversorAltura.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TddNetCoreDev.Domain.Models;
+using Xunit;
+
+
+namespace TDDWithNetCoreXUnit.Test.ConversaoAltura
+{
+    public class TestesConversorAltura
+    {
+        //[Fact] é mais usadoquando queremos testarsomente um cenário.
+        [Theory]
+        [InlineData(1, 0.3048)]
+        [InlineData(10, 3.048)]
+        [InlineData(55.5555, 16.9333)]
+        [InlineData(100, 30.48)]
+        public void TesteConversorAltura(double pes, double metros)
+        {
+            ConversorAltura conversor = new ConversorAltura();
+            double resultado = conversor.PesParaMetros(pes);
+
+            //Assert.Equal(metro, resultado);
+            resultado.Should().Be(metros, "Altura em metros não corresponde ao valor esperado de " + metros);
+        }
+    }
+}
